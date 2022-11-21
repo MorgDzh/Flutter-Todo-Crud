8e2ff1eef119a4b762719ab871394cd6ff768b87
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-11-21 19:37:23 +0600

    Initial commit :octocat:

---
+++ lib/screens/todo_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo List"),
+      ),
+    );
+  }
+}
